f68689329dc60ac9e8f7784313f483be9fa5f8a2
Wisara8 <srandall845@gmail.com>
2018-06-20 20:17:03 +0000

    rendering tweets dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,29 +3,116 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-function createTweetElement(article) {
-var $tweet = $(article).addClass("tweeted");
-return $tweet;
-}
+$('#document').ready(function(e) {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  function createTweetElement(tweetData) {
+
+    let $tweet = $('<article>').addClass("tweeted");
+
+      let $header = $('<header>').addClass('userHeader');
+      let $img = $('<img>').attr('src', tweetData.user.avatars.small);
+      $header.append($img);
+
+    let $name = $('<h2>').text(tweetData.user.name); // + tweetData.user.name + '</h2>');
+    $header.append($name);
+
+    let $handle = $('<p>').text(tweetData.user.handle);
+    $header.append($handle);
+
+    $tweet.append($header);
+
+    let $div = $('<div>').text(tweetData.content.text);
+
+    $tweet.append($div);
+
+    let $footer = $('<footer>').addClass('userFooter');
+    let $created = $('<p>').text('Created at: ' + tweetData.created_at);
+    $footer.append($created);
+
+    let $flagIcon= $('<i>').addClass('fa fa-flag');
+    let $heartIcon= $('<i>').addClass('fa fa-heart');
+    let $retweetIcon= $('<i>').addClass('fa fa-retweet');
+    let $footerIcons = $('<span>').addClass('icons');
+      $footerIcons.append($flagIcon);
+      $footerIcons.append($heartIcon);
+      $footerIcons.append($retweetIcon);
+
+    $footer.append($footerIcons);
+    $tweet.append($footer);
+
+    return $tweet;
+  }
+
+
+
+  const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+  ]
 
-var $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
+
+  function renderTweets(tweets) {
+    tweets.forEach(tweet => {
+      var $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+     })
+  }
+
+  // function renderTweets(tweet) {
+    // for (var msg on tweet) {
+      // let createdTweet = createTweetElement(mes);
+      // $(#tweets-container).append(createdTweet);
+    // }
+  // }
+
+
+  // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet);
+
+  renderTweets(tweetData);
+});
+
